7da468409b672f797e574491daeff2b6e8939b76
Ethan Shaw <ethanshaw707@gmail.com>
2025-03-26 12:22:10 -0600

    work

--- screens/App.js
+++ screens/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import AppIndex from "../app/index";
+import firebaseApp from "../firebaseConfig"; // Import Firebase configuration
 
 export default function App() {
+  console.log("Firebase initialized:", firebaseApp.name); // Log Firebase app name
   console.log("Rendering App.js");
   return <AppIndex />;
 }
